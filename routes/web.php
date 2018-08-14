e1a69433f36c449f28bc38fce8dd8cfb95e064c6
Prasad Honavar <prasad@ajency.in>
2018-08-14 13:33:34 +0530

    2. Create a model to hold User data  - id  - email ...other user credential fields Used Laravel auth

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,6 @@ Route::get('/', function () {
 Route::get('/user-profiles', function () {
     return view('userprofiles');
 });
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
