7cf8ee141e6cd71d432fa8777e2b75efcee41d84
Prasad Honavar <prasad@ajency.in>
2018-08-14 15:44:50 +0530

    reate a model to hold User profile data - age - dob ....you can add additional fields

---
+++ app/Profile.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Profile extends Model
+{
+    protected $fillable = [
+        'dob',
+    ];
+
+    protected $appends = [
+        'age',
+    ];
+
+    public function getAgeAttribute()
+    {
+        return Carbon::parse($this->attributes['dob'])->age;
+    }
+
+
+    public function user()
+    {
+        return $this->hasOne('App\User');
+    }
+
+}
